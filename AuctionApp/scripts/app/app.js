b3a63176926418a7799bae8bc65c8e3181364032
Conor Hayes <ch1987@live.ie>
2015-02-06 00:03:16 +0000

    Added icons, Ionic, FontAwesome etc. Created layout, side menu. Added Angular integration in app.js and index.html. Used Sass to alter UI code.

---
+++ AuctionApp/scripts/app/app.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module('AuctionApp', [
+    'ionic',
+]);
+
+app.controller('ContentCtrl', ['$scope', '$ionicSideMenuDelegate', '$ionicSlideBoxDelegate', function ($scope, $ionicSideMenuDelegate, $ionicSlideBoxDelegate) {
+    $scope.toggleLeft = function () {
+        $ionicSideMenuDelegate.toggleLeft();
+    };
+    $scope.toggleRight = function () {
+        $ionicSideMenuDelegate.toggleRight();
+    };
+}]);
