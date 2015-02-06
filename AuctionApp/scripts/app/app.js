8787336f25d1e9ecc1347f975a122dc6e584762c
Conor Hayes <ch1987@live.ie>
2015-02-06 00:13:19 +0000

    Added some css for UI. Added side menu template.

--- AuctionApp/scripts/app/app.js
+++ AuctionApp/scripts/app/app.js
@@ -2,6 +2,10 @@
     'ionic',
 ]);
 
+app.controller('MainCtrl', ['$scope', '$location', '$routeParams', function ($scope, $location, $routeParams) {
+    
+}]);
+
 app.controller('ContentCtrl', ['$scope', '$ionicSideMenuDelegate', '$ionicSlideBoxDelegate', function ($scope, $ionicSideMenuDelegate, $ionicSlideBoxDelegate) {
     $scope.toggleLeft = function () {
         $ionicSideMenuDelegate.toggleLeft();
@@ -10,3 +14,11 @@ app.controller('ContentCtrl', ['$scope', '$ionicSideMenuDelegate', '$ionicSlideB
         $ionicSideMenuDelegate.toggleRight();
     };
 }]);
+
+app.directive('sideMenu', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/side-menu.html',
+        controller: 'MainCtrl'
+    };
+});
