f684dea9532fe0d8d609f6a8bd504ca4f1ddfd92
Conor Hayes <ch1987@live.ie>
2015-02-06 00:35:03 +0000

    Added directives.js. Moved directives from app.js to this file for clarity.

--- AuctionApp/scripts/app/app.js
+++ AuctionApp/scripts/app/app.js
@@ -1,4 +1,5 @@
 ﻿var app = angular.module('AuctionApp', [
+    'directives',
     'ngRoute',
     'ionic',
     'firebase'
@@ -37,64 +38,3 @@ app.controller('ContentCtrl', ['$scope', '$ionicSideMenuDelegate', '$ionicSlideB
         $ionicSideMenuDelegate.toggleRight();
     };
 }]);
-
-app.directive('sideMenu', function () {
-    return {
-        restrict: 'E',
-        templateUrl: 'templates/side-menu.html',
-        controller: 'MainCtrl'
-    };
-});
-
-app.directive('productDescription', function () {
-    return {
-        restrict: 'E',
-        templateUrl: 'templates/product-description.html'
-    };
-});
-
-app.directive('sellerAvgRating', function () {
-    return {
-        restrict: 'E',
-        scope: {
-            reviews: '=reviews',
-            max: '=max'
-        },
-        templateUrl: 'templates/seller-avg-rating.html',
-        link: function (scope, elements, attr) {
-            scope.totalRating = 0;
-            scope.totalReviews = scope.reviews.length;
-            scope.average = 0;
-            scope.stars = [];
-            angular.forEach(scope.reviews, function (value, key) {
-                scope.totalRating += value.rating;
-            });
-
-            scope.average = Math.ceil(scope.totalRating / scope.totalReviews);
-
-            scope.updateStars = function () {
-                var i = 0;
-                scope.stars = [];
-                for (i = 0; i < scope.max; i += 1) {
-                    scope.stars.push({
-                        full: scope.average > i
-                    });
-                }
-            };
-
-            scope.starClass = function (star, i) {
-                var starClass = 'ion-ios7-star-outline';
-                if (star.full || i <= scope.hoverIdx) {
-                    starClass = 'ion-ios7-star';
-                }
-                return starClass;
-            };
-
-            scope.$watch('reviews', function (newValue, oldValue) {
-                if (newValue !== null && newValue !== undefined) {
-                    scope.updateStars();
-                }
-            });
-        }
-    };
-});
